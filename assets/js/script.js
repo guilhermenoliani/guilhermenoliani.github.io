09e49566a48b897301a9ae22b937454e9492f60f
Guilherme <guilhermenoliani@gmail.com>
2022-05-19 18:54:56 -0300

    💫 Efeito máquina de escrever

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,27 @@
 const qS = (el) => document.querySelector(el);
 const qsAll = (el) => document.querySelectorAll(el);
 
+//Evento de click do logo, para voltar ao topo da tela
 qS("#header--container-logo img").addEventListener("click", () => {
-  window.scrollTo(0);
+  window.scrollTo(0, 0);
+});
+
+//Função para efeito máquina de escrever HOME
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 75 * i);
+  });
+}
+const titulo = document.querySelector(".typewriter");
+typeWrite(titulo);
+
+//Evento de click para ser direcionado ao portfólio
+qS("#visitrepository button").addEventListener("click", () => {
+  window.open("https://github.com/guilhermenoliani");
 });
 
 projetosJson.map((item, index) => {
